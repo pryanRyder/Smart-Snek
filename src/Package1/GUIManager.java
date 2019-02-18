6ba59cbc9b05512dab90eaf8c55d9cff42ae80fd
Danny <38340314+moo32@users.noreply.github.com>
2019-02-18 13:01:10 -0500

    Update GUIManager.java
    updated GUIManger

--- src/Package1/GUIManager.java
+++ src/Package1/GUIManager.java
@@ -1,5 +1,8 @@
 package Package1;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -7,33 +10,41 @@ import javafx.stage.Stage;
  * @version 1.0
  * @created 17-Feb-2019 5:39:59 PM
  */
-public class GUIManager {
+public class GUIManager extends Application
+{
 
 	private ControlPane myControl;
 	private DisplayPane myDisplay;
 	private GamePane myGame;
 	private HelpTabPane myHelp;
 
-	public GUIManager(){
+	public GUIManager()
+	{
 
 	}
 
-	public void finalize() throws Throwable {
+	public void finalize() throws Throwable 
+	{
 
 	}
-	/**
-	 * 
-	 * @param args
-	 */
-	public void main(String[] args){
-
-	}
-
 	/**
 	 * 
 	 * @param primaryStage
 	 */
-	public void start(Stage primaryStage){
-
+	public void start(Stage primaryStage)
+	{
+		Pane pane = new Pane();
+		Scene scene = new Scene(pane, 500, 500);
+		
+		
+		primaryStage.setTitle("Smart-Snek");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
+	
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
 }//end GUIManager
