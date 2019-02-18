34577915727fed775eca1dbb2f919e98192cc735
Paul <pryanryder89@yahoo.com>
2019-02-18 13:49:38 -0500

    Update ControlPane.java
    My commit.

--- src/Package1/ControlPane.java
+++ src/Package1/ControlPane.java
@@ -1,12 +1,13 @@
 package Package1;
 
+import javafx.scene.layout.Pane;
 
 /**
  * @author Danny
  * @version 1.0
  * @created 17-Feb-2019 5:39:58 PM
  */
-public class ControlPane extends GUIManager {
+public class ControlPane extends Pane {
 
 
 
