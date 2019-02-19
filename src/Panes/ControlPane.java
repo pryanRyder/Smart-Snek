0769463c665e2917cdf3e0d40b79635626a3d12a
Danny <38340314+moo32@users.noreply.github.com>
2019-02-19 11:56:32 -0500

    Update ControlPane.java

--- src/Panes/ControlPane.java
+++ src/Panes/ControlPane.java
@@ -31,7 +31,7 @@ public class ControlPane extends Pane {
 		content.setStyle("-fx-background-color: '#a5a5a5'");
 		getChildren().add(content);
 		Text Title = new Text("Control Panel");
-		Title.setStyle("-fx-font-size: 20;");
+		Title.setStyle("-fx-font-size: 20;"); // NEEDS TO BE CHANGED BASED ON SIZE
 		Title.setLayoutX(content.getPrefWidth()*0.04);
 		Title.setLayoutY(content.getPrefHeight()*0.04);
 		//Title.setLayoutX(value);
