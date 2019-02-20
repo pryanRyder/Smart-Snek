577365ee04c449d9a4ca5eb1cb4d443750274718
Paul <pryanryder89@yahoo.com>
2019-02-19 19:05:40 -0500

    Update GamePane.java
    Added a tile pane. Not sure how to access each individual node. Probably will end up doing something else.

--- src/Panes/GamePane.java
+++ src/Panes/GamePane.java
@@ -1,7 +1,15 @@
 package Panes;
 
 import Snake.SnakeManager;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 /**
@@ -13,8 +21,7 @@ public class GamePane extends Pane {
 
 	public SnakeManager m_SnakeManager;
 
-
-
+	
 	public void finalize() throws Throwable {
 		super.finalize();
 	}
@@ -29,12 +36,35 @@ public class GamePane extends Pane {
 		setLayoutY(0);
 		setStyle("-fx-background-color: '#6d6d6d';");
 		
+	
+		TilePane tilepane = new TilePane();
+		
+				tilepane.setPrefColumns(20);
+			
+				tilepane.setPadding(new Insets(5,5,5,5));
+
+				
+			for(int x = 0; x<240; x++) {
+				Rectangle rec = new Rectangle();
+				rec.setHeight(28);
+				rec.setWidth(28);
+				rec.setFill(Color.DARKCYAN);
+				tilepane.getChildren().add(rec);
+			}
+			
+		    tilepane.setHgap(2);
+		    tilepane.setVgap(2);
+		    tilepane.relocate(80.0, 60.0);
+		    tilepane.setBackground(new Background(new BackgroundFill(Color.AQUAMARINE, null, null)));
+		   
+		 
 		Pane content = new Pane();
 		content.setPrefSize(getPrefWidth()*0.90, getPrefHeight()*0.90);
 		content.setLayoutX(getPrefWidth()*0.05);
 		content.setLayoutY(getPrefHeight()*0.05);
 		content.setStyle("-fx-background-color: 'black'");
-		getChildren().add(content);
+		
+		getChildren().addAll(content,tilepane);
 
 	}
 }//end GamePane
